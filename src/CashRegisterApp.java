1fb3cffb1603aa40134b268ba61cc932d0607f7e
CSys1293 <clarasysling@gmail.com>
2016-11-09 10:17:24 -0500

    updated main

--- src/CashRegisterApp.java
+++ src/CashRegisterApp.java
@@ -1,11 +1,25 @@
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 // Team Clara, Jake, Taher,
 
 public class CashRegisterApp {
 
 	public static void main(String[] args) { 
+		Scanner scanner = new Scanner(System.in);
+		Validator val = new Validator();
+		
+		System.out.println("Welcome to Elk Mouth Goods");
+		System.out.println("What are you shopping for today?");
+		
+		
+		
+		String choice1;
+		String choice2;
+		String choice3;
+		String choice4;
+		
 		ArrayList<Product> products = new ArrayList<Product>();
 
 		Reader list = new Reader();
@@ -20,14 +34,53 @@ public class CashRegisterApp {
 		tempArray.add("Shoes \t Trail Shoes \t 99.00");
 		
 		
-		System.out.println("Welcome to Elk Mouth Goods");
 		
-		System.out.println("What are you shopping for today?");
+		
 		
 		System.out.println("1. Camping, 2. Outerwear, 3. Water Sports, 4. Shoes: ");
 		//take user input(validate int 1 2 3 or 4)
-		
+		int userSelection = scanner.nextInt();
+		int userItem; 
 		//display items in selected category
+		//camping option
+		if(userSelection == 1) {
+			//prompt user for input
+			System.out.println("Select an item: ");
+			//items from category camping
+			System.out.println("1. Tent");
+			System.out.println("2. Sleeping bag");
+			System.out.println("3. Lantern");
+			System.out.println("4. Camp Stove");
+			}
+		//if user selects outerwear
+		else if (userSelection == 2) {
+			//prompt user for input
+			System.out.println("Select an item: ");
+			//items from category outerwear
+			System.out.println("1. Jacket");
+			System.out.println("2. Winter Coat");
+			System.out.println("3. Snowsuit");
+			}
+		else if(userSelection == 3 ) {
+			//prompt user for input
+			System.out.println("Select an item: ");
+			//items from cat 3 water sports
+			System.out.println("1. Kayak");
+			System.out.println("2. Oars");
+			System.out.println("3. Fishing Poles");
+			System.out.println("4. Water Skis");
+			System.out.println("5. Canoe");
+		}
+		else if(userSelection == 4) {
+			//prompt user for input
+			System.out.println("Select an item: ");
+			// items from cat shoes
+			System.out.println("1. Hiking Boots");
+			System.out.println("2. Trail Shoes");
+			System.out.println("3. Snowboots");
+			System.out.println("4. Rainboots");
+			
+		}
 		//ask for qtny for selected item
 		//display line total price * qtny
 		//continue shopping or check out?
