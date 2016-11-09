e821a6d4791cbc2c8ca2f1e6d858aafeab1360b4
Jacob Kander <kanderj@umich.edu>
2016-11-09 12:00:03 -0500

    Update cash register app with external item list

--- src/CashRegisterApp.java
+++ src/CashRegisterApp.java
@@ -6,105 +6,66 @@ import java.util.Scanner;
 
 public class CashRegisterApp {
 
-	public static void main(String[] args) { 
+	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		Validator val = new Validator();
-		
+		ArrayList<Product> productsForSale = Reader.FileReading();
+		String category = null;
+
 		System.out.println("Welcome to Elk Mouth Goods");
 		System.out.println("What are you shopping for today?");
-
-			
-		String choice1;
-		String choice2;
-		String choice3;
-		String choice4;
-		
-		ArrayList<Product> productsForSale = Reader.FileReading();
-
-		System.out.print(productsForSale.toString());
-
-		
-		
-		boolean condition = true;
-		//loop until checkout
-		do  {
-		System.out.println("Select a category: ");
-		
 		System.out.println("1. Camping, 2. Outerwear, 3. Water Sports, 4. Shoes: ");
-		//take user input(validate int 1 2 3 or 4)
+
+		// take user input(validate int 1 2 3 or 4)
+
 		int userSelection = scanner.nextInt();
-		
-		//display items in selected category
-		//camping option
-		if(userSelection == 1) {
-			//prompt user for input
-			System.out.println("Camping");
-			System.out.println("Select an item: ");
-			//items from category camping
-			System.out.println("1. Tent");
-			System.out.println("2. Sleeping bag");
-			System.out.println("3. Lantern");
-			System.out.println("4. Camp Stove");
-			int userItem = scanner.nextInt();
-			}
-		//if user selects outerwear
-		else if (userSelection == 2) {
-			//prompt user for input
-			System.out.println("Outerwear");
-			System.out.println("Select an item: ");
-			//items from category outerwear
-			System.out.println("1. Jacket");
-			System.out.println("2. Winter Coat");
-			System.out.println("3. Snowsuit");
-			}
-		else if(userSelection == 3 ) {
-			//prompt user for input
-			System.out.println("Water Sports");
-			System.out.println("Select an item: ");
-			//items from cat 3 water sports
-			System.out.println("1. Kayak");
-			System.out.println("2. Oars");
-			System.out.println("3. Fishing Poles");
-			System.out.println("4. Water Skis");
-			System.out.println("5. Canoe");
+
+		if (userSelection == 1) {
+			category = "Camping";
+		} else if (userSelection == 2) {
+			category = "Outerwear";
+		} else if (userSelection == 3) {
+			category = "Water Sports";
+		} else if (userSelection == 4) {
+			category = "Shoes";
 		}
-		else if(userSelection == 4) {
-			//prompt user for input
-			System.out.println("Shoes");
-			System.out.println("Select an item: ");
-			// items from cat shoes
-			System.out.println("1. Hiking Boots");
-			System.out.println("2. Trail Shoes");
-			System.out.println("3. Snowboots");
-			System.out.println("4. Rainboots");
-			
+
+		System.out.println("These are our " + category + " items:");
+		for (Product p : productsForSale) {
+			if (p.getCategory().toString().equalsIgnoreCase(category)) {
+				System.out.println(p.getName() + " ~ " + p.getPrice());
+			}
 		}
-		//ask for qtny for selected item
+		System.out.print("Which would you like? ");
+		int userItem = scanner.nextInt();
 		System.out.println("How many woud you like to buy?");
-		//display line total price * qtny
-		//continue shopping or check out?
-		
-		//display subtotal, tax, grandtotal "use big decimal"
-		
-		//payment type
-		
-		//display receipt 
-		System.out.println();
-		
-		//start over
-		
+		int qty = scanner.nextInt();
 
-	} while (condition);
-		
-	System.out.println("Would you like to continue shopping?");
-	
-
+		System.out.println("Would you like to continue shopping?");
 		System.out.println("Thank you for shopping at Elk Mouth Goods");
 	}
 
-	private static void printnewPriceList() {
-		// TODO Auto-generated method stub
-		
-	}
+	// boolean condition = true;
+	// loop until checkout
+
+	// if user selects outerwear
+
+	// ask for qtny for selected item
+
+	// display line total price * qtny
+	// continue shopping or check out?
+
+	// display subtotal, tax, grandtotal "use big decimal"
+
+	// payment type
+
+	// display receipt
+	// System.out.println();
+
+	// start over
+
+	// } while (condition);
+
+	// private static void printnewPriceList() {
 
 }
